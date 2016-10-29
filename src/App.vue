c48a24f51db7ee56083bcb6e9c89e2df2d9ec2a7
Blake Contreras <shardsofsilver@gmail.com>
2016-10-28 19:59:55 -0700

    Add dashboard component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
+    <dashboard></dashboard>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello';
+import Dashboard from './components/Dashboard';
 
 export default {
   name: 'app',
   components: {
-    Hello,
+    Dashboard,
   },
 };
 </script>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div>
+    <div v-for="recommendation in recommendations">
+      {{ recommendation }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      recommendations: ['game1', 'game2'],
+    };
+  },
+  computed: {},
+  ready() {},
+  attached() {},
+  methods: {},
+  components: {},
+};
+</script>
+
+<style lang="css">
+</style>
