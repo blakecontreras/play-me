b8f07e50c7c8c4bec9a8b735cbfc389c9400d493
Blake Contreras <shardsofsilver@gmail.com>
2016-11-09 15:48:43 -0800

    Add ability to search Giant Bomb API

--- server/api-handler.js
+++ server/api-handler.js
@@ -1,12 +1,38 @@
 require('dotenv').config()
+var uri = require('urijs')
+var axios = require('axios')
 
-var giantBomb = process.env.GIANT_BOMB
-var searchQuery = "http://www.giantbomb.com/api/search/?"
-var gameQuery = "http://www.giantbomb.com/api/game/?"
+// static giantbomb API info
+// response will be in the form of response.data Object with results array
+  // e.g. response.data.results
+var giantBomb = {
+  apiKey: process.env.GIANT_BOMB,
+  protocol: "http",
+  hostname: "www.giantbomb.com",
+  searchPath: "/api/search/",
+  gamePath: "/api/game/"
+}
 
 module.exports = {
-  searchGiantBomb(params) {
 
+  // params: Object with properties:
+    // field_list: list of field names to include in response, delimited with ","
+    // query: text string to query, wrapped in double quotes
+    // resources: type of media to search
+  searchGiantBomb(params) {
+    // api_key first, then format: json, then query params
+    var query = uri.buildQuery(Object.assign({
+      api_key: giantBomb.apiKey,
+      format: 'json'
+    }, params))
+    var apiQuery = uri.build({
+      protocol: giantBomb.protocol,
+      hostname: giantBomb.hostname,
+      path: giantBomb.searchPath,
+      query: query
+    })
+
+    axios.get(apiQuery).then((response) => console.log(response))
   },
   /* params: object with properties:
       title: title of game,
@@ -16,3 +42,5 @@ module.exports = {
 
   }
 }
+
+module.exports.searchGiantBomb({format: 'json', query: '"metroid prime"', resources: 'game'});
