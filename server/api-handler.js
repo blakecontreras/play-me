b73be13c8764fbd0d773087631a3838cf5bb941f
Blake Contreras <shardsofsilver@gmail.com>
2016-11-08 17:27:23 -0800

    Add basic routing

---
+++ server/api-handler.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+var giantBomb = process.env.GIANT_BOMB
+var searchQuery = "http://www.giantbomb.com/api/search/?"
+var gameQuery = "http://www.giantbomb.com/api/game/?"
+
+module.exports = {
+  searchGiantBomb(params) {
+
+  },
+  /* params: object with properties:
+      title: title of game,
+      ...
+  */
+  getGameFromGiantBomb(params) {
+
+  }
+}
--- server/router.js
+++ server/router.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var api = require('./api-handler');
 
 router.get('/hello', function(req, res) {
   res.send("Hello world");
