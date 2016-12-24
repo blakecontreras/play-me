fb5947474a4f0b603f1699e71f679eaa9782c4ad
Blake Contreras <shardsofsilver@gmail.com>
2016-12-23 19:23:47 -0800

    Add Vuex to app.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import App from './App';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+import App from './App'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
+Vue.use(Vuex)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App },
-});
+  components: { App }
+})
