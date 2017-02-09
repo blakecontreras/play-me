7f4bcf4c83f7648c60179346342c092a3322c441
Blake Contreras <shardsofsilver@gmail.com>
2017-02-08 20:30:43 -0800

    Create CurrentGame component.

---
+++ src/components/CurrentGame.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="current-game">
+    <img v-bind:src="currentGame.image">
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['currentGame']
+  }
+</script>
+
+<style>
+
+</style>
